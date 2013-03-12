f7f0c1c1d8a4030a0229a843b93a567f8187d4dc
Daniel Nguyen <pedantic@gmail.com>
2013-03-12 01:44:54 -0500

    Initial add

---
+++ Disconnect.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import argparse
+import time
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('hour', type=int)
+    parser.add_argument('minutes', type=int)
+    args = parser.parse_args()
+
+    return 60 * (60 * args.hour + args.minutes)
+
+def print_time(seconds=None):
+    if (seconds == None):
+        s = time.time()
+    else:
+        s = seconds
+
+
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(s)))
+
+
+def main():
+    seconds = parse_args()
+    interval = 89
+
+    print('Sleeping until: ')
+    print_time(seconds + time.time())
+
+    while (seconds > interval): 
+        print('Sleeping for {0} second(s)'.format(seconds))
+        print_time()
+        time.sleep(interval)
+        seconds -= interval
+
+    print('Sleeping for {0} seconds(s)'.format(seconds))
+    print_time()
+    time.sleep(seconds)
+
+    print_time()
+
+
+
+
+if __name__ == '__main__':
+    main()
